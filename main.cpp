20ed9da4aeec497dd923233d07b622b569d34710
Dan <daniyar.burakaev@mail.ru>
2019-12-23 15:32:03 +0300

    RAZUMOVSKY, YOU F***

--- main.cpp
+++ main.cpp
@@ -3,45 +3,53 @@
 #include <unistd.h>     // pipe
 using namespace std;
 
-char if1_n[] = "input1.txt";
-char if2_n[] = "input2.txt";
-char of1_n[] = "output.txt";
-
 char sub_prog_name[] = "./sub_prog";
+
 
 int main(int argc, char* argv[]) {
     FILE* merge_file;
     pid_t pid1, pid2;
     int filedes[2];
     char buff;
-    
-    merge_file = fopen(of1_n, "w");
+    int opened_files = 2;
+
+    merge_file = fopen(argv[1], "w");
     pipe(filedes);
-    
-    
+
+
     if((pid1 = fork()) == 0)
-        execl(sub_prog_name, sub_prog_name, &filedes[1], if1_n, NULL);
+        execl(sub_prog_name, sub_prog_name, &filedes[1], argv[2], NULL);
     else if(pid1 < 0)
         return -1;
-    
+
     if((pid2 = fork()) == 0)
-        execl(sub_prog_name, sub_prog_name, &filedes[1], if2_n, NULL);
+        execl(sub_prog_name, sub_prog_name, &filedes[1], argv[3], NULL);
     else if(pid2 < 0)
         return -1;
-    
-    
+
+    cout << endl << endl << "[Subprocesses created]" << endl;
     close(filedes[1]);
-    
-    cout << endl << "File content:" << endl << endl << "\t - - - - -" << endl;
-    
-    while(read(filedes[0], &buff, 1)) {
-        cout << buff;
-        fwrite(&buff, sizeof(buff), 1, merge_file);
+
+    cout << endl << "Pipe content:" << endl << endl << "\t - - - - -" << endl;
+
+    while(true) {
+        int temp = read(filedes[0], &buff, 1);
+
+        if (temp && buff == '\0')
+            opened_files--;
+        else if (temp) {
+            cout << buff;
+            fwrite(&buff, sizeof(buff), 1, merge_file);
+        }
+        else if (!temp && opened_files > 0)
+            usleep(10000);
+        else break;
+
     }
-    
+
     cout << "\t - - - - -" << endl << endl << "[Terminating...]" << endl;
     close(filedes[0]);
     fclose(merge_file);
-    
+
     return 0;
 }
--- sub_prog.cpp
+++ sub_prog.cpp
@@ -8,19 +8,22 @@ using namespace std;
 int main(int argc, char* argv[]) {
     ifstream source_file;
     string bufline;
-    
+
     int write_to_pipe_des = *argv[1];
     source_file.open(argv[2], std::ifstream::in);
-    
+
     while(!source_file.eof()) {
         getline(source_file, bufline);
         bufline.append("\n");
         size_t sz = bufline.size();
-        
+
         write(write_to_pipe_des, bufline.c_str(), sz);
-        // cout << sz << " " << bufline.c_str() << endl;
     }
-    
+
+    char t = '\0';
+    write(write_to_pipe_des, &t, 1);
+
+    cout << "[Subprogram terminating]" << endl;
     close(write_to_pipe_des);
     source_file.close();
     exit(0);
